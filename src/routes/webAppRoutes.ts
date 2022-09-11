4a63d57c6fe7b3d20293f8d2160cfced2ce9adae
neria <neria@spectory.com>
2022-09-11 14:03:06 +0300

    get user

--- src/routes/webAppRoutes.ts
+++ src/routes/webAppRoutes.ts
@@ -11,6 +11,7 @@ const router = express.Router()
 // users
 router.get('/user/login', usersController.userLogin)
 router.get('/user/getAll', usersController.getAllCompanyUsers)
+router.get('/user/getOne', usersController.getUser)
 
 // notifications
 router.get('/notifications/getRecent', notificationController.getRecentNotifications)
