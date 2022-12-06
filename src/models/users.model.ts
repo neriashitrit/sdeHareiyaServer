02fc59b2d186f2f1a2920d10eeccf540777f318c
neria <neria@spectory.com>
2022-12-06 10:51:37 +0200

    fix imports

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,5 +1,4 @@
 import DbService from '../services/db.service'
-import { TRUSTNET_SCHEMA, TRUSTNET_TABLES } from '../constants'
 
 import _ from 'lodash'
 import { IUser } from 'types'
@@ -14,11 +13,7 @@ export default class UserModel {
   getUser = async (email: string): Promise<any> =>{
     const last_login = new Date()
     try {
-      const user = await this.db.update(TRUSTNET_SCHEMA,TRUSTNET_TABLES.USERS,{last_login},{email})                             
-      if (user[0]){
-        const userImage = await this.db.getOne(TRUSTNET_SCHEMA,TRUSTNET_TABLES.IMAGE,{id:user[0].image_id})      
-        user[0].url = userImage?.url
-      }
+      const user = await this.db.update('users',{last_login},{email})                             
       return user?.[0]
     } catch (error) {
       console.error('ERROR in users.modal getUser()', error.message);
@@ -26,13 +21,7 @@ export default class UserModel {
   }
 
   createUser = async (newUser: any): Promise<IUser> =>{
-    const user = await this.db.insert(TRUSTNET_SCHEMA,TRUSTNET_TABLES.USERS, newUser)
-    return user
-  }
-
-  getAllCompanyUsers  = async (company_id: number): Promise<IUser[]> =>{
-    const users = await this.db.getAll(TRUSTNET_SCHEMA,TRUSTNET_TABLES.USERS, {company_id})
-    console.log(users)
-    return users
+    const user = await this.db.insert('users', newUser)
+    return user?.[0]
   }
 }
