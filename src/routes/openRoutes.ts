0307e9ea72ff9161d2ee22b5bdf1a83428b32275
neria <neria@spectory.com>
2022-12-11 10:09:47 +0200

    connect DB and improve health check

--- src/routes/openRoutes.ts
+++ src/routes/openRoutes.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import DbService from '../services/db.service'
 
 
 const router = express.Router()
 const healthCheck = async (req: express.Request, res:  express.Response) => {
-    return res.status(200).send("I am alive!!!")
+    const db = new DbService()
+    const DBStatus = await db.getOne('healthCheck') 
+    return res.status(200).send({server:"alive", Database:DBStatus?.status})
 }
 
 // healthCheck
