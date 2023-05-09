60193c21ba41436efe46dc14d606bca7406b5e52
neria <neria@spectory.com>
2023-05-09 15:19:33 +0300

    disable file service

--- src/controllers/global.controller.ts
+++ src/controllers/global.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import FileService from '../services/storage.service';
 import { successResponse } from '../utils/db.utils';
 
-const fileService = FileService.getInstance();
+// const fileService = FileService.getInstance();
 
 export const healthCheck = async (req: Request, res: Response) => {
   return res.status(200).json(successResponse({ server: 'alive' }));
