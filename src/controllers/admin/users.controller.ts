ed751603088be80303b00af403d6ffd33636092b
neria <neria@spectory.com>
2023-05-18 19:12:04 +0300

    after pr

--- src/controllers/admin/users.controller.ts
+++ src/controllers/admin/users.controller.ts
@@ -20,7 +20,7 @@ export const getAllUsers = async (req: Request, res: Response) => {
 export const getUserById = async (req: Request, res: Response) => {
   console.log('in controller getUserById');
   const { userId } = req.body;
-  if (!userId) return res.status(400).json(failureResponse('missing Params'));
+  if (!userId) res.status(400).json(failureResponse('missing Params'));
   try {
     const user = await userModel.getUserById(Number(userId));
     console.log(user);
@@ -34,7 +34,7 @@ export const getUserById = async (req: Request, res: Response) => {
 export const createAdminUser = async (req: Request, res: Response) => {
   console.log('in controller createAdminUser');
   const {firstName, lastName, email, phone} = req.body;
-  if (!firstName || !lastName || !email || !phone) return res.status(400).json(failureResponse('missing Params'));
+  if (!firstName || !lastName || !email || !phone) res.status(400).json(failureResponse('missing Params'));
   try {
     const ADUser = await createAdminUserInB2C(firstName, lastName, email, phone);
     const user  =  await usersHelper.createAdminUserFromADRespond(ADUser)
