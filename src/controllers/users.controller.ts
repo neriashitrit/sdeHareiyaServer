4f766215457f045381dbf31f54bb500692d59833
neria <neria@spectory.com>
2023-11-16 18:01:45 +0200

    create user start

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -3,18 +3,18 @@ import { Request, Response } from 'express'
 import { EmailTemplateName, emailSubjectMapping } from '../constants'
 import globalHelper from '../helpers/global.helper'
 import usersHelper from '../helpers/users.helper'
+import { userModel } from '../models/users.model'
 
 const appUrl = process.env.APP_URL!
 
 
-export const getUser = (req: Request, res: Response) => {
-  // try {
-  //   const user = req.user as IUser
-
-  //   return res.status(200).json(successResponse(user))
-  // } catch (error) {
-  //   return res.status(500).json(failureResponse(error))
-  // }
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const user = await userModel.getUser()
+    return res.status(200).json((user))
+  } catch (error) {
+    return res.status(500).json((error))
+  }
 }
 
 export const updateUser = async (req: Request, res: Response) => {
