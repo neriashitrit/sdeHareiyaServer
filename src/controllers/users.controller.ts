6a841f7c271ff90b9138e409cc95d89134d1ecd2
neria <neria@spectory.com>
2024-05-06 15:49:15 +0300

    send when user exist

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -40,7 +40,7 @@ export const createUser = async (req: Request, res: Response) => {
     return res.status(200).json((createdUser))
   } catch (error) {
     console.log('in contriker user ', error)
-    if(error.errorCode == 4011) return res.status(401).json((error))
+    if(error.error.errorCode == 4011) return res.status(401).json((error))
     return res.status(500).json((error))
   }
 }
